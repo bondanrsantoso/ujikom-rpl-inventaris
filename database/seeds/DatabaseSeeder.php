914883c359d8e25f36ba99e010b7ea4c7cec6d21
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2019-03-07 22:31:54 +0700

    Doing Bootstrap and stuff fiddling with the look

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserLevelSeeder::class);
+        if(env("APP_DEBUG")){
+            $this->call(DefaultAdminSeeder::class);
+        }
     }
 }
